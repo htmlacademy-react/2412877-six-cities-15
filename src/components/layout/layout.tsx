a2de055ea901bcdbbc4373a39a05772acb7922f4
Art <102982@inbox.ru>
2024-02-27 00:12:44 +0500

    Фикс линтера

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -42,14 +42,14 @@ function Layout(): JSX.Element {
                     <Link className="header__nav-link header__nav-link--profile" to={AppRoutes.favorite}>
                       <div className="header__avatar-wrapper user__avatar-wrapper">
                       </div>
-                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                      <span className="header__user-name user__name">HostOfForest@gmail.com</span>
                       <span className="header__favorite-count">3</span>
                     </Link>
                   </li>
                   <li className="header__nav-item">
-                    <a className="header__nav-link" href="#">
+                    <Link className="header__nav-link" to={AppRoutes.login}>
                       <span className="header__signout">Sign out</span>
-                    </a>
+                    </Link>
                   </li>
                 </ul>
               </nav>

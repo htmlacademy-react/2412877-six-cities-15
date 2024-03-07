a2fd38668a70a60008c10319b4762a3039d19382
Art <102982@inbox.ru>
2024-03-07 20:14:15 +0500

    фикс ошибки на странице предложения

---
+++ src/components/offers-list/offers-list.tsx
@@ -0,0 +1,16 @@
+import Card from '../../components/card/card.tsx';
+import { TCard } from '../mock/types.tsx';
+
+type OffersListProps = {
+  cards: TCard[];
+}
+function OffersList({cards}: OffersListProps): JSX.Element {
+  const shownCards = cards.slice(0, 3);
+  return (
+    <div className="near-places__list places__list">
+      {shownCards.map((card) => <Card card={card} className='near-places' key={card.id} />)}
+    </div>
+  );
+}
+
+export default OffersList;

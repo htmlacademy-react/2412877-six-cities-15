0d46b2f8ef12febfac529d66177da77627838ce9
Art <102982@inbox.ru>
2024-03-26 20:37:19 +0500

    Обнуление для поля ввода рейтинга, добавлено

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -6,18 +6,19 @@ import { postCommentToOffer } from '../../store/api-actions.ts';
 type InputItemProps = {
   value: string;
   title: string;
+  checkedValue: string;
   onInputChange: ReactEventHandler<HTMLInputElement | HTMLTextAreaElement>;
 }
 
 export type FormDataType = {
-  rating: number;
+  rating: string;
   review: string;
 }
 
-function InputItem({value, title, onInputChange}: InputItemProps): JSX.Element {
+function InputItem({value, title, checkedValue, onInputChange}: InputItemProps): JSX.Element {
   return (
     <>
-      <input className="form__rating-input visually-hidden" name="rating" value={value} id={`${value}-stars`} type="radio" onChange={onInputChange}/>
+      <input className="form__rating-input visually-hidden" name="rating" value={value} id={`${value}-stars`} type="radio" onChange={onInputChange} checked={checkedValue === value} />
       <label htmlFor={`${value}-stars`} className="reviews__rating-label form__rating-label" title={title}>
         <svg className="form__star-image" width="37" height="33">
           <use xlinkHref="#icon-star"></use>
@@ -29,10 +30,11 @@ function InputItem({value, title, onInputChange}: InputItemProps): JSX.Element {
 
 function ReviewForm(): JSX.Element {
   const offerId = useAppSelector((state) => state.offer.offerInfo?.id);
+  const isPostReviewError = useAppSelector((state) => state.offer.isPostReviewError);
   const dispatch = useAppDispatch();
 
   const [formData, setFormData] = useState<FormDataType>({
-    rating: 0,
+    rating: '0',
     review: ''
   });
 
@@ -48,10 +50,12 @@ function ReviewForm(): JSX.Element {
         id: offerId,
         comment: formData
       }));
-      setFormData({
-        rating: 0,
-        review: ''
-      });
+      if (!isPostReviewError) {
+        setFormData({
+          rating: '0',
+          review: ''
+        });
+      }
     }
   };
 
@@ -59,14 +63,14 @@ function ReviewForm(): JSX.Element {
     <form className="reviews__form form" action="#" method="post" onSubmit={handleFormSubmit}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        {Object.entries(RatingNames).map(([rate, title]) => <InputItem value={rate} title={title} key={title} onInputChange={handleFormChange}/>).reverse()}
+        {Object.entries(RatingNames).map(([rate, title]) => <InputItem value={rate} title={title} key={title} onInputChange={handleFormChange} checkedValue={formData.rating}/>).reverse()}
       </div>
       <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" onChange={handleFormChange} value={formData.review}></textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">{CommentLength.MIN} characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled={formData.rating === 0 || formData.review.length < CommentLength.MIN || formData.review.length > CommentLength.MAX}>Submit</button>
+        <button className="reviews__submit form__submit button" type="submit" disabled={formData.rating === '0' || formData.review.length < CommentLength.MIN || formData.review.length > CommentLength.MAX}>Submit</button>
       </div>
     </form>
   );
